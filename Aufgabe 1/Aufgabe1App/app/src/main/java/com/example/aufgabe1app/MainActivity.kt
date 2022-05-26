643e16c0eb2df169e2640b1a74bfdeaa2ce39a85
TSA0101 <thore.h.sanders@gmail.com>
2022-05-26 03:04:12 +0200

    Add sample project for assignment 1

---
+++ Aufgabe 1/Aufgabe1App/app/src/main/java/com/example/aufgabe1app/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.aufgabe1app
+
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mediaPlayer : MediaPlayer
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mediaPlayer = MediaPlayer.create(this, R.raw.sample_12s)
+        mediaPlayer.isLooping = true
+        mediaPlayer.start()
+
+        val winButton = findViewById<Button>(R.id.winButton)
+        winButton.setOnClickListener {
+            val intent = Intent(this, WinActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
